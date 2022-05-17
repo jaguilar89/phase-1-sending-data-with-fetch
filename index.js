66b7cea888ba1511ce04b7b56285ee0cf5fa8c16
Jose Aguilar <jaguilarny@gmail.com>
2022-05-17 14:09:25 -0400

    commit tree 7b865909cea4227a7b080b4329efde47816b8d5b

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+}
+
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const h1 = document.createElement('h1')
+        h1.textContent = `${data.id}`
+        document.body.appendChild(h1)
+    })
+    .catch(error => {
+        const h1 = document.createElement('h1')
+        h1.textContent = `${error.message}`
+        document.body.appendChild(h1)
+    })
+}
